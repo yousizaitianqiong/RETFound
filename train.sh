7489af0620932ea30c22baf7102fd6179618ec03
rmaphoh <rmaphoh@ucl.ac.uk>
2025-08-31 18:07:38 +0100

    Incorporate DINOv3, DINOv2

--- train.sh
+++ train.sh
@@ -1,17 +1,18 @@
 # ==== Model settings ====
+# adaptation {finetune,lp}
 ADAPTATION="finetune"
-MODEL="Dinov2"
-MODEL_ARCH="dinov2_vitl14"
-FINETUNE="dinov2_vitl14_pretrain.pth"
+MODEL="RETFound_dinov2"
+MODEL_ARCH="retfound_dinov2"
+FINETUNE="RETFound_dinov2_meh"
 
 # ==== Data settings ====
+# change the dataset name and corresponding class number
 DATASET="MESSIDOR2"
 NUM_CLASS=5
-
-data_path="/home/jupyter/public_dataset/${DATASET}"
+data_path="./${DATASET}"
 task="${MODEL_ARCH}_${DATASET}_${ADAPTATION}"
 
-CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=1 --master_port=48766 main_finetune.py \
+torchrun --nproc_per_node=1 --master_port=48766 main_finetune.py \
   --model "${MODEL}" \
   --model_arch "${MODEL_ARCH}" \
   --finetune "${FINETUNE}" \
